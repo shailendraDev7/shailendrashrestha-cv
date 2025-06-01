91ca2032619681f78fc7f158530dcff3737fa969
Shailendra Shrestha <csd.solutiongate@gmail.com>
2025-06-01 19:47:27 +0545

    Adding config for both dev and prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/shailendraDev7/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'index.js',
-        assetFileNames: '[name].[ext]'
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_URL || "/", // Dynamically set base
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "index.js",
+          assetFileNames: "[name].[ext]"
+        }
       }
     }
-  }
+  };
 });
