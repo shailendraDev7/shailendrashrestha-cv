395b5b695d1bd98561831b15617c17c9976dbcee
Shailendra Shrestha <csd.solutiongate@gmail.com>
2025-06-01 17:59:21 +0545

    Added dist to .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:' https://github.com/shailendraDev7/shailendrashrestha-cv.git',
+  base:' https://github.com/shailendraDev7/',
 });
