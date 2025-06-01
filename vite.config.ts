95eebe6e2a3cdfb120a15998f0538bea31bca58a
Shailendra Shrestha <csd.solutiongate@gmail.com>
2025-06-01 20:32:44 +0545

    Readded dist and gh-pages deploy:

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react(), tailwindcss()],
-    base: env.VITE_BASE_URL || "/", // Dynamically set base
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: "index.js",
-          assetFileNames: "[name].[ext]"
-        }
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/shailendrashrestha-cv"
 });
