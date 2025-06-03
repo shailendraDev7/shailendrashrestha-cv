689aaeb2c583a867e3972a2a582a8d95681e1eb1
Shailendra Shrestha <csd.solutiongate@gmail.com>
2025-06-03 11:49:53 +0545

    Completed frontend section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/shailendrashrestha-cv">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/shailendrashrestha-cv"
+  build: {
+    sourcemap: false, // Prevents TypeScript files from being exposed
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js",
+        assetFileNames: "[name].[ext]"
+      }
+    }
+  },
+  base: "/shailendrashrestha-cv/"
 });
